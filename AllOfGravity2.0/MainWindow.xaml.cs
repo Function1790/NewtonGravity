365938759a0eaabea8fe92468f6c4f21962cdc3f
TerryFX <67085909+Function1790@users.noreply.github.com>
2022-06-06 11:44:21 +0900

    Add files via upload

---
+++ AllOfGravity2.0/MainWindow.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AllOfGravity2._0
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Constnat
+        const double GRAVITY_CONSTANT = 10;
+
+        //Variable
+
+        //Velocity Variable
+        bool isHeldLeftButton = false;
+        Vector startHeldPosition;
+        Line VelocityViewer = new Line();
+
+        //Systen Variable
+        List<Thing> ExperimentObjects = new List<Thing>();
+        DispatcherTimer NewtonEngin = new DispatcherTimer();
+        
+        Random Rand;
+
+        //Main
+        void main()
+        {
+            Log("Start", "Loading");
+            Rand = new Random();
+            NewtonEngin.Interval = TimeSpan.FromMilliseconds(1);
+            NewtonEngin.Tick += new EventHandler(NewtonEngin_Tick);
+
+            /*
+            ExperimentObjects.Add(ObjectA);
+            ExperimentObjects.Add(ObjectB);
+            ExperimentObjects.Add(ObjectC);
+            ExperimentObjects.Add(ObjectD);
+            ExperimentObjects.Add(ObjectE);
+            ExperimentObjects.Add(ObjectF);
+            */
+
+            Log("Finish", "Loading");
+            FinishedLoad();
+        }
+
+        //About System
+        void Log(string title, object content)
+        {
+            //Console.WriteLine($"[{title}] >> {content}");
+        }
+
+        void print(object content)
+        {
+            Console.WriteLine(content);
+        }
+
+        /// <summary>
+        /// await(new Action(delegate {}))
+        /// </summary>
+        private void AwaitCode(Action code)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate { }));
+        }
+
+        void FinishedLoad()
+        {
+            NewtonEngin.Start();
+        }
+
+        //Operation Engin
+        /*/
+         * 최적화 필요
+        /*/
+        double Square(double n) { return n * n; }
+
+        void GravityForceBetween(Thing A, Thing B)
+        {
+            Vector CenterA = A.Center;
+            Vector CenterB = B.Center;
+            double Δx = -(CenterA.X - CenterB.X);
+            double Δy = -(CenterA.Y - CenterB.Y);
+            double r = Math.Sqrt(Square(Δx) + Square(Δy));
+            
+
+            double Fg = (GRAVITY_CONSTANT * A.Mass * B.Mass) / Square(r);
+
+            //about A
+            double sinA = Δy / r;
+            double cosA = Δx / r;
+
+            Vector F = new Vector(Fg * cosA, Fg * sinA);
+
+            A.ApplyForce(F);
+            B.ApplyForce(-F);
+
+            //Console.WriteLine(F);
+        }
+
+        void NewtonEngin_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ExperimentObjects.Count; i++)
+            {
+                ExperimentObjects[i].Move();
+                //ExperimentObjects[i].life--;
+                //if (ExperimentObjects.Count - 1 == i) continue;
+                for (int j = i + 1; j < ExperimentObjects.Count; j++)
+                {
+                    GravityForceBetween(ExperimentObjects[i], ExperimentObjects[j]);
+                }
+            }
+            /*for (int i = ExperimentObjects.Count - 1; i >= 0; i--)
+            {
+                if (ExperimentObjects[i].life <= 0)
+                {
+                    WorldGrid.Children.Remove(ExperimentObjects[i]);
+                    ExperimentObjects.RemoveAt(i);
+                }
+            }*/
+        }  
+
+        //About Object
+        void OnCreatingNewObject(Point mp)
+        {
+            isHeldLeftButton = false;
+            int mass = Convert.ToInt32(InputMass.Text);
+            Vector applySpeed = (toVector(mp) - startHeldPosition)/10;
+            
+            if (mass > 0)
+            {
+                CreateNewExperimentObject(toPoint(startHeldPosition), mass, applySpeed);
+            }
+            else
+            {
+                Log("ValueError", "InputMass.Text <= 0");
+            }
+        }
+
+        void CreateNewExperimentObject(Point pos, int mass, Vector speed)
+        {
+            int n = ExperimentObjects.Count;
+            ExperimentObjects.Add(new Thing());
+
+            double halfOfMass = mass / 2;
+            if (mass > 100)
+            {
+                halfOfMass = 0;
+            }
+            ExperimentObjects[n].Margin = new Thickness(pos.X - halfOfMass, pos.Y - halfOfMass, 0, 0);
+            ExperimentObjects[n].Mass = mass;
+            byte R = (byte)Rand.Next(0, 255);
+            byte G = (byte)Rand.Next(0, 255);
+            byte B = (byte)Rand.Next(0, 255);
+            ExperimentObjects[n].BackGroundColor = Color.FromRgb(R, G, B);
+            ExperimentObjects[n].Velocity = speed;
+
+            WorldGrid.Children.Add(ExperimentObjects[n]);
+
+            Log("Create", $"Object[{n}] -> Mass : {mass}\tPosition : ({ExperimentObjects[n].Position})");
+        }
+
+        void CancelDrawingVelocityViwer()
+        {
+            isHeldLeftButton = false;
+            WorldGrid_Canvas.Children.Remove(VelocityViewer);
+        }
+
+        //Convert Unit
+        Vector toVector(Point p)
+        {
+            return new Vector(p.X, p.Y);
+        }
+        
+        Point toPoint(Vector p)
+        {
+            return new Point(p.X, p.Y);
+        }
+
+        //Event
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            main();
+        }
+
+        private void WorldGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point mp = e.GetPosition(WorldGrid);
+                startHeldPosition = toVector(mp);
+                isHeldLeftButton = true;
+
+                VelocityViewer = new Line();
+                VelocityViewer.X1 = mp.X;
+                VelocityViewer.Y1 = mp.Y;
+                VelocityViewer.Stroke = Brushes.Red;
+                VelocityViewer.StrokeThickness = 1;
+                VelocityViewer.StrokeDashArray = null;
+
+                WorldGrid_Canvas.Children.Add(VelocityViewer);
+            }
+            if (e.LeftButton == MouseButtonState.Released && isHeldLeftButton)
+            {
+                CancelDrawingVelocityViwer();
+            }
+        }
+
+        private void WorldGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isHeldLeftButton)
+            {
+                if(e.LeftButton == MouseButtonState.Released)
+                {
+                    CancelDrawingVelocityViwer();
+                }
+                Point mp = e.GetPosition(WorldGrid);
+                VelocityViewer.X2 = mp.X;
+                VelocityViewer.Y2 = mp.Y;
+            }
+            
+        }
+
+        private void WorldGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released && isHeldLeftButton)
+            {
+                Point mp = e.GetPosition(WorldGrid);
+                try
+                {
+                    OnCreatingNewObject(mp);
+                    WorldGrid_Canvas.Children.Remove(VelocityViewer);
+                }
+                catch (Exception err)
+                {
+                    CancelDrawingVelocityViwer();
+                    Log("Error", err.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ AllOfGravity2.0/Thing.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AllOfGravity2._0
+{
+    //Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate { loadData(); }));
+    /// <summary>
+    /// Thing.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Thing : UserControl
+    {
+        Vector Speed = new Vector(0, 0);
+        double mass = 10;
+
+        public int life = 1000;
+
+        void Log(string title, object content)
+        {
+            Console.WriteLine($"[{title}] >> {content}");
+        }
+
+        /// <summary>
+        /// await(new Action(delegate {}))
+        /// </summary>
+        private void AwaitCode(Action code)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(delegate {  }));
+        }
+
+        public Thing()
+        {
+            InitializeComponent();
+        }
+
+        double absolute(double value)
+        {
+            if (value < 0) return -value;
+            return value;
+        }
+
+        public Color BackGroundColor
+        {
+            set
+            {
+                BG.Fill = new SolidColorBrush(Color.FromArgb(30, value.R, value.G, value.B));
+                BG.Stroke = new SolidColorBrush(value);
+            }
+        }
+
+        public bool DebugMode
+        {
+            set
+            {
+                if (value)
+                {
+                    VectorX.Visibility = Visibility.Visible;
+                    VectorY.Visibility = Visibility.Visible;
+                    DebugViewer.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    VectorX.Visibility = Visibility.Hidden;
+                    VectorY.Visibility = Visibility.Hidden;
+                    DebugViewer.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+
+        void UpdateDebugData()
+        {
+            DebugViewer.Text = "[ mass ]\n";
+            DebugViewer.Text += $"{mass}\n";
+            DebugViewer.Text += "[ velocity ]\n";
+            DebugViewer.Text += $"{toString(Speed)}\n";
+            DebugViewer.Text += "[ position ]\n";
+            DebugViewer.Text += $"{toString(Position)}\n";
+        }
+
+        public void ApplyForce(Vector F)
+        {
+            //Log("Apply", $"V{Velocity}\tF {F}");
+            if(absolute(F.X) < 1000 && F.X != 0)   Vx += F.X / Mass;
+            if(absolute(F.Y) < 1000 && F.Y != 0)   Vy += F.Y / Mass;
+            //Log("Apply", $"V {Velocity}");
+        }
+
+        string toString(Vector v)
+        {
+            return String.Format("({0:0.00}, {1:0.00})", v.X, v.Y);
+        }
+
+        Vector CopyVector(Vector toCopy)
+        {
+            return new Vector(toCopy.X, toCopy.Y);
+        }
+
+        public bool Move()
+        {
+            if(Speed.X == 0 & Speed.Y == 0)
+            {
+                return false;
+            }
+
+            //Log("M", $"S:{Speed}, P:{Position}");
+            Position += Speed / 10;
+            //Log("M2", $"S:{Speed}, P:{Position}");
+            if (Position.X < 0)
+            {
+                Margin = new Thickness(0, Margin.Top, 0, 0);
+            }
+            else if(Position.X > 2300)
+            {
+                Margin = new Thickness(2300, Margin.Top, 0, 0);
+            }
+            if (Position.Y < 0)
+            {
+                Margin = new Thickness(Margin.Left, 0, 0, 0);
+            }
+            else if (Position.Y > 1300)
+            {
+                Margin = new Thickness(Margin.Left, 1300, 0, 0);
+            }
+
+            return true;
+        }
+
+        // GET SET
+        public double Mass
+        {
+            get
+            {
+                return mass;
+            }
+            set
+            {
+                mass = value;
+                Width = value;
+                Height = value;
+                if(value > 100)
+                {
+                    Width = 100 + value / 100;
+                    Height = Width;
+                }
+                UpdateDebugData();
+            }
+        }
+
+        public Vector Position
+        {
+            get
+            {
+                return new Vector(Margin.Left, Margin.Top);
+            }
+            set
+            {
+                //*Await하기
+               // Log("V", $"S:{Speed}, P:{value.ToString()}");
+                Margin = new Thickness(value.X, value.Y, 0, 0);
+                UpdateDebugData();
+            }
+        }
+
+        public Vector Center
+        {
+            get
+            {
+                return this.Position/2;
+            }
+        }
+
+        public Vector Velocity
+        {
+            get
+            {
+                return Speed;
+            }
+            set
+            {
+                Speed = CopyVector(value);
+                UpdateDebugData();
+            }
+        }
+
+        /// <summary>
+        /// Speed X
+        /// </summary>
+        public double Vx
+        {
+            get
+            {
+                return Speed.X;
+            }
+            set
+            {
+                Speed.X = value;
+                UpdateDebugData();
+            }
+        }
+        
+        /// <summary>
+        /// Speed Y
+        /// </summary>
+        public double Vy
+        {
+            get
+            {
+                return Speed.Y;
+            }
+            set
+            {
+                Speed.Y = value;
+                UpdateDebugData();
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void UserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
